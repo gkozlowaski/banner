d8b363f0921b9f6312bb59f4ee3d7f2d9d192c07
Breno Calazans <breno@vtex.com.br>
2015-07-14 11:15:17 -0300

    Refactor CSS code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,8 @@
 import storefront from 'storefront';
 import React from 'react';
 import connectToStores from 'utils/connectToStores';
+import classNames from 'classnames';
+import style from '../style/Banner.less'; // eslint-disable-line
 
 @connectToStores([
   storefront.flux.stores.ShopStore,
@@ -22,14 +24,19 @@ class Banner extends React.Component {
   render() {
     const editMode = this.props.EditorStore.get('edit');
     const component = this.props.ComponentStore.getIn([this.props.route, this.props.id]).toJS();
-    const imageStyle = editMode ? { backgroundColor: 'rgba(0,0,200,0.5)', opacity: '0.2' } : {};
+    const src = component.settings.url;
+
+    let classes = classNames(
+      'vtex-storefront-banner',
+      { 'vtex-storefront-banner--edit': editMode }
+    );
+
     if (!component) {
       return null;
     }
-    const src = component.settings.url;
     return (
-      <div className="storefront-banner" style={{width: '100%'}} onClick={this.onClickContainer.bind(this)}>
-        <img style={imageStyle} src={src} width="100%"/>
+      <div className={classes} onClick={this.onClickContainer.bind(this)}>
+        <img src={src} width="100%"/>
       </div>
     );
   }
