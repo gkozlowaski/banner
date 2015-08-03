330614cc990b2f959693f02e45e6bb7cf9374d1b
Daniel Fosco <danielfosco@VTEXs-MacBook-Pro-2.local>
2015-08-03 11:35:10 -0300

    Vastly improve the layout for BannerEditor -- further improvements needed to finish the interaction and experience

--- src/editors/BannerEditor.jsx
+++ src/editors/BannerEditor.jsx
@@ -36,28 +36,30 @@ class BannerEditor extends React.Component {
 
     return (
       <div className="v-banner-ed__editor">
-      <h1 className="v-banner-ed__title">Banner</h1>
-      <div className="v-banner-ed__current-banner">
-      <span className="v-banner-ed__banner-placeholder-text">Nenhuma imagem selecionada</span>
-      </div>
-      <div className="v-banner-ed__add">
-        <button id="add" className="v-banner-ed__add-button"></button>
-        <label htmlFor="add" className="v-banner-ed__add-label">Adicionar nova imagem</label>
-      </div>
-        <form className="v-banner-ed__form">
-            <div className="v-banner-ed__form__url">
-              <label htmlFor="url">URL da imagem</label>
-              <input id="url" className="form-control" type="text"
-                     value={this.state.url} onChange={this.changeUrl} placeholder="URL"/>
-            </div>
-            <div className="v-banner-ed__form__alt">
-              <label htmlFor="alt">Alt-text da imagem</label>
-              <input id="alt" className="form-control" type="text"
-                    onChange={this.changeAlt} placeholder="Alt-text"/>
-            </div>
-        </form>
-        <ActionBar onSave={this.onClickSave}/>
-      </div>
+        <div className="v-banner-ed__editor__wrapper">
+          <h1 className="v-banner-ed__title">Banner</h1>
+          <div className="v-banner-ed__current-banner">
+          <span className="v-banner-ed__banner-placeholder-text">Nenhuma imagem selecionada</span>
+          </div>
+          <div className="v-banner-ed__add">
+            <button id="add" className="v-banner-ed__add-button"></button>
+            <label htmlFor="add" className="v-banner-ed__add-label">Adicionar nova imagem</label>
+          </div>
+            <form className="v-banner-ed__form">
+                <div className="v-banner-ed__form__url">
+                  <label htmlFor="url">URL da imagem</label>
+                  <input id="url" className="form-control" type="text"
+                         value={this.state.url} onChange={this.changeUrl} placeholder="URL"/>
+                </div>
+                <div className="v-banner-ed__form__alt">
+                  <label htmlFor="alt">Alt-text da imagem</label>
+                  <input id="alt" className="form-control" type="text"
+                        onChange={this.changeAlt} placeholder="Alt-text"/>
+                </div>
+            </form>
+          </div>
+          <ActionBar onSave={this.onClickSave}/>
+        </div>
     );
   }
 }
