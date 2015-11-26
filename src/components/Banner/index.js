cc9290243a56291c0924ef61044df8365407edd0
leandrooriente <leandroriente@gmail.com>
2015-11-26 16:33:29 -0200

    Render component with mock data

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,22 +2,62 @@ import React from 'react';
 import { editable } from 'vtex-editor';
 import './style.scss';
 import BannerImage from './BannerImage';
+import Slider from 'react-slick';
+import 'assets/slick/slick.scss';
+import 'assets/slick/slick-theme.scss';
 
 @editable({
   name: 'Banner@vtex.banner',
   title: 'Banner'
 })
 class Banner extends React.Component {
-  render() {
-    let settings;
-    if (this.props.settings) {
-      settings = this.props.settings.toJS();
+  // MOCK
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: {
+        slider: {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+        images: [
+          {
+            url: 'http://i.imgur.com/7ou79ca.png',
+            link: 'http://www.google.com',
+            alt: 'Olar ma frend'
+          },
+          {
+            url: 'http://i.imgur.com/7ou79ca.png',
+            link: 'http://www.google.com',
+            alt: 'Olar ma frend'
+          },
+          {
+            url: 'http://i.imgur.com/7ou79ca.png',
+            link: 'http://www.google.com',
+            alt: 'Olar ma frend'
+          }
+        ]
+      }
     }
+  }
+
+  render() {
+    let images = this.state.settings.images;
+    let sliderSettings = this.state.settings.slider;
 
     return (
-      <div className="Banner">
-        <BannerImage {...settings}/>
-      </div>
+      <Slider {...sliderSettings}>
+        {images.map(function(image){
+          return (
+            <div>
+              <BannerImage imageUrl={image.url} link={image.link} altText={image.alt}/>
+            </div>
+          )
+        })}
+      </Slider>
     );
   }
 }
