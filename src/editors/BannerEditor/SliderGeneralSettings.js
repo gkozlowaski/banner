f94e7fe0d9bbf2230a3fc8e94980a1227e0f9d89
Thor Amorim <thoramorim@gmail.com>
2016-05-10 06:29:02 -0300

    Rework SliderGeneralSettings

--- src/editors/BannerEditor/SliderGeneralSettings.js
+++ src/editors/BannerEditor/SliderGeneralSettings.js
@@ -1,67 +1,117 @@
 import React from 'react';
 
 class SliderGeneralSettings extends React.Component {
-  constructor(props) {
-    super(props);
+  changeDots = () => {
+    const dots = !this.props.settings.dots;
+    const slider = Object.assign({}, this.props.settings, { dots });
+    this.props.updateSettings({ slider });
   }
 
-  stringToBoolean (string) {
-    return string === 'true';
+  changeInfinite = () => {
+    const infinite = !this.props.settings.infinite;
+    const slider = Object.assign({}, this.props.settings, { infinite });
+    this.props.updateSettings({ slider });
   }
 
-  changeDots (e) {
-    let dots = this.stringToBoolean(e.target.value);
-    let slider = Object.assign({}, this.props.settings, { dots });
-    this.props.updateSettings({slider});
+  changeSpeed = (e) => {
+    const speed = parseInt(e.target.value, 10);
+
+    if (speed < 0) {
+      return;
+    }
+
+    const slider = Object.assign({}, this.props.settings, { speed });
+    this.props.updateSettings({ slider });
   }
 
-  changeInfinite (e) {
-    let infinite = this.stringToBoolean(e.target.value);
-    let slider = Object.assign({}, this.props.settings, { infinite });
-    this.props.updateSettings({slider});
+  incrementQuantity = (e) => {
+    e.preventDefault();
+    const speed = this.props.settings.speed + 50;
+    const slider = Object.assign({}, this.props.settings, { speed });
+    this.props.updateSettings({ slider });
   }
 
-  changeSpeed (e) {
-    let speed = e.target.value;
-    let slider = Object.assign({}, this.props.settings, { speed });
-    this.props.updateSettings({slider});
+  decrementQuantity = (e) => {
+    e.preventDefault();
+    const speed = this.props.settings.speed - 50;
+    const slider = Object.assign({}, this.props.settings, { speed });
+    this.props.updateSettings({ slider });
   }
 
   render() {
-    let settings = this.props.settings;
+    const settings = this.props.settings;
 
     return (
-      <form className="v-banner-ed__form">
-        <div className="v-banner-ed__form__wrapper">
-          <h2>Configurações do Slider</h2>
-          <div className="v-banner-ed__form__slider-dots">
-            <h4>Exibir pontos para navegação?</h4>
-            <label className="radio-inline">
-              <input type="radio" name="slider-dots" id="slider-dots-yes" checked={settings.dots} onChange={this.changeDots.bind(this)} value="true" />
-              Sim
-            </label>
-            <label className="radio-inline">
-              <input type="radio" name="slider-dots" id="slider-dots-false" checked={!settings.dots} onChange={this.changeDots.bind(this)} value="false" />
-              Não
-            </label>
+      <form className="BannerEditor__form-wrapper">
+        <div className="BannerEditor__form">
+          <h2 className="BannerEditor__title--big">
+            Slider
+          </h2>
+          <div>
+            <span className="BannerEditor__title">
+              Selection Dots
+            </span>
+            <input
+              type="checkbox"
+              name="slider-dots"
+              id="slider-dots-yes"
+              className="BannerEditor__toggle BannerEditor__toggle--light"
+              checked={settings.dots}
+              onChange={() => {}}
+            />
+            <label
+              className="BannerEditor__toggle-button"
+              for="slider-dots-yes"
+              onClick={this.changeDots}
+            />
           </div>
-
-          <div className="v-banner-ed__form__slider-infinite">
-            <h4>Retornar ao primeiro item ao chegar no final?</h4>
-            <label className="radio-inline">
-              <input type="radio" name="infinite" id="infinite-yes" checked={settings.infinite} onChange={this.changeInfinite.bind(this)} value="true" />
-              Sim
-            </label>
-            <label className="radio-inline">
-              <input type="radio" name="infinite" id="infinite-false" checked={!settings.infinite} onChange={this.changeInfinite.bind(this)} value="false" />
-              Não
-            </label>
+          <br />
+          <div>
+            <span className="BannerEditor__title">
+              Scroll infinito
+            </span>
+            <input
+              type="checkbox"
+              name="slider-dots"
+              id="infinite-yes"
+              className="BannerEditor__toggle BannerEditor__toggle--light"
+              checked={settings.infinite}
+              onChange={() => {}}
+            />
+            <label
+              className="BannerEditor__toggle-button"
+              for="infinite-yes"
+              onClick={this.changeInfinite}
+            />
           </div>
-
-          <div className="v-banner-ed__form__slider-speed">
-            <h4>Velocidade da transição em milisegundos</h4>
-            <input id="speed" className="form-control" type="number"
-                  value={settings.speed} onChange={this.changeSpeed.bind(this)} />
+          <br />
+          <div className="BannerEditor__quant-field">
+            <p className="BannerEditor__title">
+              Transition Speed (ms)
+            </p>
+            <div className="BannerEditor__quant-selector">
+              <button
+                className="BannerEditor__quant-button--left"
+                onClick={this.decrementQuantity}
+                disabled={settings.speed === 0}
+              >
+                -
+              </button>
+              <input
+                id="speed"
+                className="BannerEditor__quant-selector__input"
+                name="quantity"
+                type="text"
+                value={settings.speed}
+                onChange={this.changeSpeed}
+              />
+              <button
+                className="BannerEditor__quant-button--right"
+                onClick={this.incrementQuantity}
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
       </form>
